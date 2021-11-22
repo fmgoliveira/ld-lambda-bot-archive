dc32d0c138340d4ec1cc16d7a2f1d66a34bb35be
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2021-11-22 11:52:00 +0000

    c

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ const client = new Client({
         "GUILD_INVITES",
         "GUILD_VOICE_STATES",
         "GUILD_MEMBERS",
-        "GUILD_PRESENCES"
+        "GUILD_PRESENCES",
+        "GUILD_BANS",
+        "GUILD_EMOJIS_AND_STICKERS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING"
     ]
 })
 
+// client.login(process.env.BETA_TOKEN)
 client.login(process.env.BOT_TOKEN)
