e104e691c461cca7d89eb92d25f07d9630767cf5
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2021-12-07 14:22:31 +0000

    commit

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -30,15 +30,7 @@ module.exports = class extends Client {
 
         const server = http.createServer((req, res) => {
             try {
-                const headers = {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-                    'Access-Control-Max-Age': 2592000, // 30 days
-                    /** add other headers as per requirement */
-                };
-                res.writeHead(204, headers);
-                res.end();
-                res.writeHead(200, {"Content-Type": "text/plain"})
+                res.writeHead(200, { "Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'OPTIONS, POST, GET', 'Access-Control-Max-Age': 2592000 })
                 res.end(`{ guilds: ${this.guilds.cache.size}, channels: ${this.channels.cache.size}, users: ${memberCountStr} }`)
             } catch {
                 res.writeHead(500, {"Content-Type": "text/plain"})
