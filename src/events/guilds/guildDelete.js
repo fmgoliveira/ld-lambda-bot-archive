fb063e68a3ddaebdd8c3b5cb79c3a33d671bc9ce
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-01-25 15:45:41 +0000

    commit

--- src/events/guilds/guildDelete.js
+++ src/events/guilds/guildDelete.js
@@ -8,24 +8,24 @@ module.exports = class extends Event {
         })
     }
 
-    run = async (guild) => {
+    run = async(guild) => {
         try {
             this.client.channels.cache.get(process.env.LAMBDA_GUILD_LOGS).send({
                 embeds: [
                     new MessageEmbed()
-                        .setTitle("Left a Guild")
-                        .setDescription(`Bot left a guild.`)
-                        .addField("Name", guild.name)
-                        .addField("ID", guild.id, true)
-                        .addField("Owner ID", guild.ownerId, true)
-                        .addField("Premium Tier (Nitro boosts)", `\`${guild.premiumTier}\``)
-                        .addField("Members", `${guild.members.cache.size}`, true)
-                        .addField("Channels", `${guild.channels.cache.size}`, true)
-                        .addField("Roles", `${guild.roles.cache.size}`, true)
-                        .setThumbnail(guild.iconURL())
-                        .setTimestamp()
-                        .setFooter(this.client.user.username, this.client.user.avatarURL())
-                        .setColor("RED")
+                    .setTitle("Left a Guild")
+                    .setDescription(`Bot left a guild.`)
+                    .addField("Name", guild.name)
+                    .addField("ID", guild.id, true)
+                    .addField("Owner ID", guild.ownerId, true)
+                    .addField("Premium Tier (Nitro boosts)", `\`${guild.premiumTier}\``)
+                    .addField("Members", `${guild.members.cache.size}`, true)
+                    .addField("Channels", `${guild.channels.cache.size}`, true)
+                    .addField("Roles", `${guild.roles.cache.size}`, true)
+                    .setThumbnail(guild.iconURL())
+                    .setTimestamp()
+                    .setFooter(this.client.user.username, this.client.user.avatarURL())
+                    .setColor("RED")
                 ]
             }).catch(err => console.log(err))
         } catch (err) {
