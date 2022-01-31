5c7c3b674ce1dd256695e88dcd43180b52e4992a
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-01-31 16:07:01 +0000

    commit

--- src/commands/general/info.js
+++ src/commands/general/info.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
             .setDescription(`Developed by <:logo:921033010764218428> **Lambda Development** Group`)
             .addField("Version", `\`${process.env.BOT_VERSION}\``)
             .addField("Creator", "<@549619189271494676> (`@DrMonocle#4948`)")
-            .addField("Links", `[Invite](https://bot.lambdadev.xyz/invite)\n[Support](https://bot.lambdadev.xyz/support)\n[Wiki](https://wiki.lambdadev.xyz/bot)\n[Dashboard](https://bot.lambdadev.xyz/dashboard)`)
+            .addField("Links", `[Invite](https://bot.lambdadev.xyz/invite)\n[Support](https://bot.lambdadev.xyz/support)\n[Wiki](https://wiki.lambdadev.xyz/bot)\n[Dashboard](https://bot.lambdadev.xyz/dashboard)\n[Upvote](https://discord.com/channels/878935240377241701/936677696300253204/937720075320979516)`)
             .setThumbnail(this.client.user.avatarURL())
             .setTimestamp()
             .setFooter("Requested by: " + message.member.user.username, this.client.user.avatarURL())
