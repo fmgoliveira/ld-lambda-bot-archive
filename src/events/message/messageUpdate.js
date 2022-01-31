383245cb0c4786482f51ff06dffc4939b17b92d4
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-01-31 16:18:46 +0000

    commit

--- src/events/message/messageUpdate.js
+++ src/events/message/messageUpdate.js
@@ -10,6 +10,7 @@ module.exports = class extends Event {
 
     run = async (oldMessage, newMessage) => {
         if (oldMessage.author.bot) return
+        if (oldMessage.content === newMessage.content) return
 
         const settings = await this.client.db.guilds.findOne({ guildId: newMessage.guild.id }) || new this.client.db.guilds({ guildId: newMessage.guild.id })
         const logging = settings.logging
@@ -21,8 +22,6 @@ module.exports = class extends Event {
         const Original = oldMessage.content.slice(0, count) + (oldMessage.content.lenght > count ? "..." : "")
         const Edited = newMessage.content.slice(0, count) + (newMessage.content.lenght > count ? "..." : "")
 
-        console.log(newMessage.url)
-
         try {
             newMessage.guild.channels.cache.get(logging.channel.messageEvents).send({
                 embeds: [
@@ -32,7 +31,7 @@ module.exports = class extends Event {
                         .addField("Old message", Original)
                         .addField("New message", Edited)
                         .addField("User", `<@${newMessage.author.id}> (${newMessage.author.tag} - \`${newMessage.author.id}\`)`)
-                        .addField(`Jump to message", "[Click here](${newMessage.url})`)
+                        .addField("Jump to message", `[Click here](${newMessage.url})`)
                         .setTimestamp()
                         .setFooter(this.client.user.username, this.client.user.avatarURL())
                         .setColor(logging.color.messageEvents)
