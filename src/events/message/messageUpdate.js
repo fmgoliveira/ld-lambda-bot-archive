90294bea04a52ca60ed414cf7330d29bc69384bc
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-01-31 10:07:16 +0000

    commit

--- src/events/message/messageUpdate.js
+++ src/events/message/messageUpdate.js
@@ -9,12 +9,17 @@ module.exports = class extends Event {
     }
 
     run = async (oldMessage, newMessage) => {
+        if (oldMessage.author.bot) return
+
         const settings = await this.client.db.guilds.findOne({ guildId: newMessage.guild.id }) || new this.client.db.guilds({ guildId: newMessage.guild.id })
         const logging = settings.logging
 
         if (!logging.active.messageEvents.messageEdit) return
-
         if (!logging.channel.messageEvents) return
+
+        const count = 1024
+        const Original = oldMessage.content.slice(0, count) + (oldMessage.content.lenght > count ? "..." : "")
+        const Edited = newMessage.content.slice(0, count) + (newMessage.content.lenght > count ? "..." : "")
 
         try {
             newMessage.guild.channels.cache.get(logging.channel.messageEvents).send({
@@ -22,8 +27,8 @@ module.exports = class extends Event {
                     new MessageEmbed()
                         .setTitle("Message edited")
                         .setDescription(`A message was updated in <#${newMessage.channel.id}>.`)
-                        .addField("Old message", oldMessage.content.length < 1024 && oldMessage.content.lenght > 0 ? oldMessage.content : oldMessage.content.substring(0, 1020) + "...")
-                        .addField("New message", newMessage.content.length < 1024 && newMessage.content.lenght > 0 ? newMessage.content : newMessage.content.substring(0, 1020) + "...")
+                        .addField("Old message", Original)
+                        .addField("New message", Edited)
                         .addField("User", `<@${newMessage.author.id}> (${newMessage.author.tag} - \`${newMessage.author.id}\`)`)
                         .addField(`Jump to message", "[Click here](${newMessage.url})`)
                         .setTimestamp()
