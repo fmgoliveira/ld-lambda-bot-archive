4cc2ab505cb632045a896723624b3eeac8a27928
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2022-02-13 10:18:09 +0000

    commit

--- commands/about/vote.js
+++ commands/about/vote.js
@@ -27,12 +27,12 @@ module.exports = {
 
         const embed = new MessageEmbed()
             .setTitle("Upvote Bot")
+            .setAuthor({ name: `Your vote status: ${getVoteStatus(userDb)}`, iconURL: interaction.member.user.avatarURL({ dynamic: true }) })
             .setDescription(`By upvoting me, you are helping the Team (a lot) and it's completely free! Each time you vote for me on the sites below, I get more users.`)
             .addField("Why should you upvote Lambda Bot?", `First of all, it's free and doesn't take a lot of time. \nSecondly, you get the following **benefits**:\n\n• Voters role in Lambda Development Discord Server\n• Cool voters badge in the \`/whois\` command and \`User Info\` context command\n• Your name listed on \`#voting\` channel in Lambda Development Discord Server\n• Access to premium-only commands\n• Bonus entry in all giveaways managed by this bot\n\n_ _`)
             .addField("What is a vote streak?", "A vote streak is __the amount of votes you have in a certain moment__ (you can have up to 4 votes simultaneously by voting in all the links below)\n\n_ _")
             .addField("What are the voters roles and badges available?", `• **1** single vote: <:voted:937668445640724480> @Voted\n• **2** votes streak: <:double_voted:937668445695266867> @Double Voted\n• **3** votes streak: <:triple_voted:937668445728800808> @Triple Voted\n• **4** votes streak: <:dominating_votes:937668445686878208> @Dominating Votes\n\n_ _`)
             .addField("Where can I upvote the bot?", `You can upvote the bot in any of the links listed below:\n\n:link: [Top.gg](https://top.gg/bot/900398063607242762/vote)\n:link: [Scarps Bot List](https://botlist.scarps.club/bots/like/900398063607242762)\n:link: [Discord Labs](https://bots.discordlabs.org/bot/900398063607242762?vote)\n:link: [Bots for Discord](https://discords.com/bots/bot/900398063607242762/vote)\n\n_ _`)
-            .addField("Your current status", `${getVoteStatus(userDb)}`)
             .setThumbnail(client.user.avatarURL())
             .setTimestamp()
             .setFooter(client.footer)
